c7fffd2a0d3dfb75b1657618a4bcc85dccf4629b
vupot123 <vupot123@gmail.com>
2024-08-29 21:57:50 +0700

    eg

---
+++ WebApplication1/NewFolder/HomeController2.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.NewFolder
+{
+    public class HomeController2 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
